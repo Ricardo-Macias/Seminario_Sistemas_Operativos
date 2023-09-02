00761dd7e73c1c7a761a48f8586f45617282c185
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-01 22:00:17 -0600

    feat: agregue la funcion hexadecimal_to_decimal en Main.py

--- Practica_1_Procesamiento_Lotes/Main.py
+++ Practica_1_Procesamiento_Lotes/Main.py
@@ -8,8 +8,12 @@ def Edit_file(line):
     with open("Practica_1_Procesamiento_Lotes//prueba.txt","a") as file:
         file.write(line)
 
+def hexadecimal_to_decimal(letter):
+    return str(int(letter,16))
+
 
 if __name__ == "__main__":
     lines = Read_file()
-    print(lines[0])
-    Edit_file("Line")
+
+    print(hexadecimal_to_decimal("A"))
+    
