45e3939cfba334da684a4e1d5f8b16e3d8ab6b2f
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-01 22:27:19 -0600

    feat: agregue la funcion Clean_IPv6 en Main.py

--- Practica_1_Procesamiento_Lotes/Main.py
+++ Practica_1_Procesamiento_Lotes/Main.py
@@ -5,7 +5,7 @@ def Read_file():
     return content
 
 def Edit_file(line):
-    with open("Practica_1_Procesamiento_Lotes//prueba.txt","a") as file:
+    with open("Practica_1_Procesamiento_Lotes//Salida.txt","a") as file:
         file.write(line)
 
 def hexadecimal_to_decimal(letter):
@@ -14,10 +14,23 @@ def hexadecimal_to_decimal(letter):
 def decimal_to_hexadecimal(number):
     return str(hex(int(number)))
 
+def Clean_IPv6(IPv6):
+    aux_IPv6 = ""
+    IPv6_to_decimal = ""
+    for count in range(len(IPv6)):
+        if IPv6[count] == "/":
+            return IPv6_to_decimal + hexadecimal_to_decimal(aux_IPv6)
+        elif IPv6[count] == ":":
+            IPv6_to_decimal += hexadecimal_to_decimal(aux_IPv6) + ":"
+            aux_IPv6 = ""
+        else:
+            aux_IPv6 += IPv6[count]
+
 
 if __name__ == "__main__":
     lines = Read_file()
 
-    print(decimal_to_hexadecimal(10))
+    line = lines[0].split(',')
+    print(Clean_IPv6(line[0]))
 
     
