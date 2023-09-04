196bac8b0a18f70ae5f1ba0cb6e9d6292ca1685b
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-03 21:15:53 -0600

    feat: agregue la funcion create_file en main.py

--- Practica_2_Procesamiento_Lotes_ll/main.py
+++ Practica_2_Procesamiento_Lotes_ll/main.py
@@ -5,3 +5,11 @@ def Read_file(name):
     with open(name, "r") as file:
         contents = file.read()
     return contents
+
+def craete_file(name,text):
+    with open(name, "a") as file:
+        file.write(text)
+
+
+if __name__ == "__main__":
+    pass
