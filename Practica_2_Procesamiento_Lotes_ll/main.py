36dbf7a6b972a2b6e6848cd2534da253eb2c4c17
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-03 21:32:26 -0600

    feat: Agregue la funcion folder_contents en main.py

--- Practica_2_Procesamiento_Lotes_ll/main.py
+++ Practica_2_Procesamiento_Lotes_ll/main.py
@@ -1,5 +1,7 @@
+import os
 
-
+list_folder = []
+list_file = []
 
 def Read_file(name):
     with open(name, "r") as file:
@@ -10,6 +12,19 @@ def craete_file(name,text):
     with open(name, "a") as file:
         file.write(text)
 
+def folder_contents():
+    list_contents = os.listdir()
+    for count_contents in range(len(list_contents)):
+        if ".txt" in list_contents[count_contents]:
+            list_file.append(list_contents[count_contents])
+        else:
+            list_folder.append(list_contents[count_contents])
+
+
+x = "D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_2_Procesamiento_Lotes_ll\\Practica"
 
 if __name__ == "__main__":
-    pass
+    os.chdir(x)
+    folder_contents()
+    print(list_file)
+    print(list_folder)
