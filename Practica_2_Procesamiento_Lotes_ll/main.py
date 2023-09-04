d2c2bccb190db799b3a69a71f42780ad786e60fe
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-04 13:39:09 -0600

    feat: agregue el proceso de navegacion y creacion de carpetas

--- Practica_2_Procesamiento_Lotes_ll/main.py
+++ Practica_2_Procesamiento_Lotes_ll/main.py
@@ -2,6 +2,8 @@ import os
 
 list_folder = []
 list_file = []
+original = []
+copies = []
 
 def Read_file(name):
     with open(name, "r") as file:
@@ -22,12 +24,54 @@ def folder_contents():
             list_file.append(list_contents[count_contents])
         else:
             list_folder.append(list_contents[count_contents])
+            original.append(os.getcwd() + "\\" + list_contents[count_contents])
 
-def Enter_folder():
-    pass
+
+def create_copies(text):
+    print(copies[0])
+    os.chdir(copies[0])
+    for count_file in range(len(list_file)):
+        create_file(list_file.pop(0),text)
+    for count_folder in range(len(list_folder)):
+        folder = list_folder.pop(0)
+        create_folder(folder)
+        copies.append(copies[0] + "\\" + folder)
+
+
+
+def Batch_processing():
+    while len(original) != 0:
+        os.chdir(original[0])
+        folder_contents()
+        if len(list_file) != 0 or len(list_folder) != 0:
+            create_copies("Hola")
+        original.pop(0)
+        copies.pop(0)
 
 x = "D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_2_Procesamiento_Lotes_ll\\Practica"
 
 if __name__ == "__main__":
+
+
     os.chdir(x)
-    create_folder("sub-carpeta_2")
+    original.append(x)
+
+    copy_folder = x.split("\\")
+    name_folder = "Copia_"  + copy_folder[len(copy_folder)-1]
+
+    copy_folder.pop()
+    copy_folder = "\\".join(copy_folder)
+    os.chdir(copy_folder)
+    create_folder(name_folder)
+
+    copy_folder = x.split("\\")
+    copy_folder.pop()
+    copy_folder.append(name_folder)
+    copy_folder = "\\".join(copy_folder) 
+
+    copies.append(copy_folder)
+
+    Batch_processing()
+
+    
+    
