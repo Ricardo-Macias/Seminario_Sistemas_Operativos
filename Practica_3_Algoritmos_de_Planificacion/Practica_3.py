5efb1fe72200c0771d435dfa9875f595cd3623a6
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-08 18:41:25 -0600

    fix: cambiar file.read por filelines

--- Practica_3_Algoritmos_de_Planificacion/Practica_3.py
+++ Practica_3_Algoritmos_de_Planificacion/Practica_3.py
@@ -2,9 +2,19 @@ import time
 
 def Read_file():
     with open("D:\\Archivos\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_3_Algoritmos_de_Planificacion\\procesos.txt", "r") as file:
-        return file.read()
+        return file.readlines()
+
+def FIFO(file):
+
+    for count_process in range(len(file)):
+        print(file[count_process])
+
+        
+
 
 
 if __name__ == "__main__":
-    process = Read_file()
-    print(process)
+    file = Read_file()
+    print(file)
+    #FIFO(file)
+    
