1a2c9739de557d2c113df7d7c0541ea07301aebc
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-08 18:47:32 -0600

    feat: agregar funcion FIFO en Practica_3.py

--- Practica_3_Algoritmos_de_Planificacion/Practica_3.py
+++ Practica_3_Algoritmos_de_Planificacion/Practica_3.py
@@ -3,18 +3,18 @@ import time
 def Read_file():
     with open("D:\\Archivos\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_3_Algoritmos_de_Planificacion\\procesos.txt", "r") as file:
         return file.readlines()
+    
 
 def FIFO(file):
 
     for count_process in range(len(file)):
-        print(file[count_process])
-
-        
-
-
+        process = file[count_process].split(",")
+        print("\nProceso: ", process[0])
+        for count_time in range(int(process[2])):
+            print(count_time + 1, ", ", end="")
+            time.sleep(1)
 
 if __name__ == "__main__":
-    file = Read_file()
-    print(file)
-    #FIFO(file)
+    file_process = Read_file()
+    FIFO(file_process)
     
