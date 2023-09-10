609ac90fe84e2fd39b15800825c0502ef05fddf9
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-10 16:28:12 -0600

    feat: agregar funcion Round_Robin en Practica_4.py

--- Practica_4_Algoritmo_de_Planificacion_ll/Practica_4.py
+++ Practica_4_Algoritmo_de_Planificacion_ll/Practica_4.py
@@ -24,6 +24,33 @@ def process(list_process,position=None):
     time.sleep(2)
     Frame_process.destroy()
 
+def Round_Robin(file):
+    Frame_process = customtkinter.CTkFrame(root, width=400, height=400)
+    Frame_process.place(x=50, y=50)
+    y = 0
+    while len(file) != 0:
+        process = file[0].split(",")
+        y += 20
+        customtkinter.CTkLabel(Frame_process, text=process[0]).place(x=10, y=y)
+        process_time = int(process[2])
+        x = 200
+        for count_quantum in range(3):
+            if process_time != 0:
+                customtkinter.CTkLabel(Frame_process, text=count_quantum + 1).place(x=x, y=y)
+                root.update()
+                x += 20
+                process_time -= 1
+                time.sleep(1)
+            else:
+                break
+        file.pop(0)
+        if process_time > 0:
+            process[2] = process_time
+            file.append(process[0] + ", " + process[1] + ", " + str(process[2]))
+    time.sleep(2)
+    Frame_process.destroy()
+        
+
 def option(option_process):
 
     if option_process == "FIFO":
@@ -33,7 +60,7 @@ def option(option_process):
     elif option_process == "Prioridad":
         process(file,1)
     elif option_process == "Round Robin":
-        Practica_3.Round_Robin(file)
+        Round_Robin(file)
  
 if __name__ == "__main__":
     file = Practica_3.Read_file()
