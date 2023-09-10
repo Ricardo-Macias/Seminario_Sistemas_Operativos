b089cf31d6b8420e6c0fca1a3ec70768dad4e263
Ricardo_Home <ricardo.macias1755@alumnos.udg.mx>
2023-09-09 19:34:36 -0600

    feat: importar la Practica_3 en Practica_4.py

---
+++ Practica_4_Algoritmo_de_Planificacion_ll/Practica_4.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos")
+from Practica_3_Algoritmos_de_Planificacion import Practica_3
+
+
+if __name__ == "__main__":
+    file = Practica_3.Read_file()
+    print(file)
