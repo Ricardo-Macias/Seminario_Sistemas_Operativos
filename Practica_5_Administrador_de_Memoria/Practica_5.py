a9e3b786944a2bf1cf3e2966aee8b6fa12539b95
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-09-18 10:41:58 -0600

    feat: agregar string_to_int en practica_5.py

--- Practica_5_Administrador_de_Memoria/Practica_5.py
+++ Practica_5_Administrador_de_Memoria/Practica_5.py
@@ -1,9 +1,19 @@
 
+memory_space = ['1000kb', '400kb', '1800kb','700kb', '900kb', '1200kb', '1500kb']
+
 def Read_file():
     with open("D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_5_Administrador_de_Memoria\\archivos.txt", "r") as file:
         content = file.readlines()
     return content
 
+def string_to_int(kb):
+    kilobytes = ""
+    for count_kb in kb:
+        if count_kb == "k":
+            return int(kilobytes)
+        else:
+            kilobytes += count_kb
+
 
 if __name__ == "__main__":
-    print(Read_file())
+    print(string_to_int(memory_space[1]))
