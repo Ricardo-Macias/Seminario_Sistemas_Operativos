43939681b3abdc41f1e431b7dbd425e496654181
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-07 18:26:24 -0600

    feat: crear ventana

---
+++ Practica_7_Productor_Consumidor_1/Practica_7.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+if __name__ == "__main__":
+    app = Tk()
+    app.geometry("500x500")
+    app.title("Practica 7: Producto Consumidor 1")
+
+    app.mainloop()
