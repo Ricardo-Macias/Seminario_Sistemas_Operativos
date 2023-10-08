b280c50774fd182d6b4a1c7bea1aa9e798518a13
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-07 18:37:25 -0600

    feat: agregar funcion loading_image en la clase car_park en Practica_7

--- Practica_7_Productor_Consumidor_1/Practica_7.py
+++ Practica_7_Productor_Consumidor_1/Practica_7.py
@@ -1,6 +1,16 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+class car_park:
+    def _init(self,app):
+        self.app = app
+
+    def loading_image(self, name, width, height):
+        img = Image.open(name)
+        new_image = img.resize(width, height)
+        return ImageTk.PhotoImage(new_image)
+
+
 if __name__ == "__main__":
     app = Tk()
     app.geometry("500x500")
