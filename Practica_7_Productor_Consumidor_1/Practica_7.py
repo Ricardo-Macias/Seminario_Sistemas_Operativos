2474e7d718b4566a4b27e4ddf1276af4443ea05f
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-07 18:44:00 -0600

    fix: Corregir linea 7 y 13

--- Practica_7_Productor_Consumidor_1/Practica_7.py
+++ Practica_7_Productor_Consumidor_1/Practica_7.py
@@ -4,12 +4,13 @@ from PIL import Image, ImageTk
 class car_park:
     def __init__(self,app):
         self.app = app
-        image_carPark = self.loading_image("Estacionamiento.png",500,500)
-        lbl_image_carPark = Label(self.app, image=image_carPark).pack()
+        image_carPark = self.loading_image(
+            "D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_7_Productor_Consumidor_1\\Estacionamiento.png", 500, 500)
+        lbl_image_carPark = Label(self.app, image=image_carPark)
 
     def loading_image(self, name, width, height):
         img = Image.open(name)
-        new_image = img.resize(width, height)
+        new_image = img.resize((width, height))
         return ImageTk.PhotoImage(new_image)
 
 
