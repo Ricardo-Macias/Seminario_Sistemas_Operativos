eff267895e87a6ae2c9e43e10589242a374aa34f
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-08 21:34:45 -0600

    feat: agregar funcion second en la clase car_park

--- Practica_7_Productor_Consumidor_1/Practica_7.py
+++ Practica_7_Productor_Consumidor_1/Practica_7.py
@@ -1,12 +1,16 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from random import randint
 
 class car_park:
     def __init__(self,app):
         self.app = app
-        image_carPark = self.loading_image(
-            "D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_7_Productor_Consumidor_1\\Estacionamiento.png", 500, 500)
-        lbl_image_carPark = Label(self.app, image=image_carPark)
+        self.time = [0.5, 1, 2]
+        self.carPark = [0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    def second(self):
+        time = randint(0,2)
+        return self.time[time]
 
     def loading_image(self, name, width, height):
         img = Image.open(name)
@@ -16,9 +20,14 @@ class car_park:
 
 if __name__ == "__main__":
     app = Tk()
-    app.geometry("500x500")
+    app.geometry("700x500")
     app.title("Practica 7: Producto Consumidor 1")
 
     carPark = car_park(app)
+
+    image_carPark = carPark.loading_image(
+        "D:\\Archivos\\Practicas\\6_Semestre\\Seminario_Sistemas_Operativos\\Practica_7_Productor_Consumidor_1\\Estacionamiento.png", 700, 500)
+    lbl_image_carPark = Label(app, image=image_carPark)
+    lbl_image_carPark.place(x=0, y=0)
 
     app.mainloop()
