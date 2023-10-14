542413b24f5a6cf8821bfd0a94cdcd1db5631ec6
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-14 17:12:16 -0600

    refactor: mover interfaz y funciones a una clase

--- Practica_8_Lector_escritor/Practica_8.py
+++ Practica_8_Lector_escritor/Practica_8.py
@@ -1,14 +1,40 @@
 import customtkinter
-from tkinter import *
+import threading
 
-def edit():
-    txt_writer.configure(state="normal")
-    txt_read.configure(state="normal")
-    txt_read_2.configure(state="normal")
+class Interfaz:
+    def __init__(self,app):
+        self.app = app
+        self.sem = threading.Semaphore(1)
+        self.text_txtbox = ""
 
-def save():
-    text_txtbox = txt_writer.get("1.0", "end-1c")
-    print(text_txtbox)
+        self.txt_writer = customtkinter.CTkTextbox(self.app, width=250, height=400)
+        self.txt_writer.place(x=10, y=50)
+        self.txt_read = customtkinter.CTkTextbox(self.app, width=250, height=400)
+        self.txt_read.place(x=270, y=50)
+        self.txt_read_2 = customtkinter.CTkTextbox(self.app, width=250, height=400)
+        self.txt_read_2.place(x=530, y=50)
+
+        self.btn_read = customtkinter.CTkButton(
+            self.app, text="Leer", width=50)
+        self.btn_read.place(x=10, y=20)
+        self.btn_save = customtkinter.CTkButton(
+            self.app, text="Guardar", width=50, command=self.save)
+        self.btn_save.place(x=70, y=20)
+        self.btn_edit = customtkinter.CTkButton(
+            self.app, text="Editar", width=50, command=self.edit)
+        self.btn_edit.place(x=140, y=20)
+
+        self.txt_writer.configure(state="disabled")
+        self.txt_read.configure(state="disabled")
+        self.txt_read_2.configure(state="disabled")
+
+    def edit(self):
+        self.txt_writer.configure(state="normal")
+
+    def save(self):
+        text_txtbox = self.txt_writer.get("1.0", "end-1c")
+        self.txt_writer.configure(state="disabled")
+
 
 
 if __name__ == "__main__":
@@ -16,24 +42,6 @@ if __name__ == "__main__":
     app.geometry("800x500")
     app.title("Practica 8: Producto Consumidor")
 
-    text_txtbox = ""
-
-    txt_writer = customtkinter.CTkTextbox(app, width=250, height=400)
-    txt_writer.place(x=10, y=50)
-    txt_read = customtkinter.CTkTextbox(app, width=250, height=400)
-    txt_read.place(x=270, y=50)
-    txt_read_2 = customtkinter.CTkTextbox(app, width=250, height=400)
-    txt_read_2.place(x=530, y=50)
-
-    btn_read = customtkinter.CTkButton(app, text="Leer", width=50)
-    btn_read.place(x=10, y=20)
-    btn_save = customtkinter.CTkButton(app, text="Guardar", width=50, command=save)
-    btn_save.place(x=70, y=20)
-    btn_edit = customtkinter.CTkButton(app, text="Editar", width=50, command=edit)
-    btn_edit.place(x=140, y=20)
-
-    txt_writer.configure(state="disabled")
-    txt_read.configure(state="disabled")
-    txt_read_2.configure(state="disabled")
+    lector_escritor = Interfaz(app)
 
     app.mainloop()
