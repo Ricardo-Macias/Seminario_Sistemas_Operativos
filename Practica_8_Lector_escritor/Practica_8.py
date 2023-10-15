8eee755f6ddfd935e4850fad7bb80bd747bd81d9
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-14 21:52:58 -0600

    fix: Correccion de errores

--- Practica_8_Lector_escritor/Practica_8.py
+++ Practica_8_Lector_escritor/Practica_8.py
@@ -4,18 +4,15 @@ import random
 import time
 
 class Interfaz:
-    def __init__(self,app):
+    def __init__(self,app, textbox):
         self.app = app
+        self.txt_read = textbox
         self.sem = threading.Semaphore(1)
         self.text_txtbox = ""
         self.letter_textbox = ""
 
-    def edit(self):
-        self.txt_writer.configure(state="normal")
-
-    def save(self):
-        self.text_txtbox = self.txt_writer.get("1.0", "end-1c")
-        self.txt_writer.configure(state="disabled")
+    def save(self,text):
+        self.text_txtbox = text
     
     def reader_writer(self):
         read = threading.Thread(name="Lector", target=self.reader)
@@ -27,19 +24,11 @@ class Interfaz:
     def reader(self):
         tm = [1, 1.5, 2]
         count = 0
-        band_1, band_2 = False, False
         while count < len(self.text_txtbox):
             if self.letter_textbox == "":
                 self.letter_textbox = self.text_txtbox[count]
                 time.sleep(tm[random.randint(0, 2)])
-                band_1 = True 
-            elif self.letter_textbox_2 == "":
-                self.letter_textbox_2 = self.text_txtbox[count]
-                time.sleep(tm[random.randint(0, 2)])
-                band_2 = True
-            if band_1 and band_2:
                 count += 1
-                band_1, band_2 = False, False
     
     def writer(self):
         tm = [0.5, 1, 2]
@@ -54,12 +43,24 @@ class Interfaz:
                 count += 1
         self.txt_read.configure(state="disabled")
 
+def edit():
+    txt_writer.configure(state="normal")
+
+def save_text():
+    txt_writer.configure(state="disabled")
+    lector_escritor.save(txt_writer.get("1.0", "end-1c"))
+    lector_escritor_2.save(txt_writer.get("1.0", "end-1c"))
+
+def reader_writer():
+    txt_read.configure(state="normal")
+    txt_read_2.configure(state="normal")
+    lector_escritor.reader_writer()
+    lector_escritor_2.reader_writer()
+
 if __name__ == "__main__":
     app = customtkinter.CTk()
     app.geometry("800x500")
     app.title("Practica 8: Producto Consumidor")
-
-    lector_escritor = Interfaz(app)
 
     txt_writer = customtkinter.CTkTextbox(app, width=250, height=400)
     txt_writer.place(x=10, y=50)
@@ -71,14 +72,17 @@ if __name__ == "__main__":
     txt_read_2.place(x=530, y=50)
 
     btn_read = customtkinter.CTkButton(
-        app, text="Leer", width=50, command=self.reader_writer)
+        app, text="Leer", width=50, command=reader_writer)
     btn_read.place(x=10, y=20)
     btn_save = customtkinter.CTkButton(
-        app, text="Guardar", width=50, command=self.save)
+        app, text="Guardar", width=50, command=save_text)
     btn_save.place(x=70, y=20)
     btn_edit = customtkinter.CTkButton(
-        app, text="Editar", width=50, command=self.edit)
+        app, text="Editar", width=50, command=edit)
     btn_edit.place(x=140, y=20)
+
+    lector_escritor = Interfaz(app, txt_read)
+    lector_escritor_2 = Interfaz(app, txt_read_2)
 
     txt_writer.configure(state="disabled")
     txt_read.configure(state="disabled")
