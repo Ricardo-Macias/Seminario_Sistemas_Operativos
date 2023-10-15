9144a74056da5e3491d202a411723d9602b6835b
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-14 19:21:24 -0600

    feat: agregar funcionamiento al segundo cuadro de texto

--- Practica_8_Lector_escritor/Practica_8.py
+++ Practica_8_Lector_escritor/Practica_8.py
@@ -8,8 +8,8 @@ class Interfaz:
         self.app = app
         self.sem = threading.Semaphore(1)
         self.text_txtbox = ""
-        self.letter_textbox_textbox = ""
-        self.letter_textbox_textbox_2 = ""
+        self.letter_textbox = ""
+        self.letter_textbox_2 = ""
 
         self.txt_writer = customtkinter.CTkTextbox(self.app, width=250, height=400)
         self.txt_writer.place(x=10, y=50)
@@ -44,22 +44,31 @@ class Interfaz:
         self.txt_read_2.configure(state="normal")
 
         read = threading.Thread(name="Lector", target=self.reader)
+        read2 = threading.Thread(name="Lector_2", target=self.reader)
         write = threading.Thread(name="Escritor", target=self.writer)
+        write2 = threading.Thread(name="Escritor_2", target=self.writer_textbox_2)
 
         read.start()
+        read2.start()
         write.start()
+        write2.start()
     
     def reader(self):
         tm = [1, 1.5, 2]
         count = 0
+        band_1, band_2 = False, False
         while count < len(self.text_txtbox):
             if self.letter_textbox == "":
-                self.sem.acquire()
                 self.letter_textbox = self.text_txtbox[count]
-
-                self.sem.release()
                 time.sleep(tm[random.randint(0, 2)])
-                count += 1   
+                band_1 = True 
+            elif self.letter_textbox_2 == "":
+                self.letter_textbox_2 = self.text_txtbox[count]
+                time.sleep(tm[random.randint(0, 2)])
+                band_2 = True
+            if band_1 and band_2:
+                count += 1
+                band_1, band_2 = False, False
     
     def writer(self):
         tm = [0.5, 1, 2]
@@ -67,14 +76,25 @@ class Interfaz:
         while count < len(self.text_txtbox):
             if self.letter_textbox != "":
                 self.sem.acquire()
-
                 self.txt_read.insert(customtkinter.END, self.letter_textbox)
                 self.letter_textbox = ""
-
                 self.sem.release()
-
                 time.sleep(tm[random.randint(0, 2)])
                 count += 1
+        self.txt_read.configure(state="disabled")
+    
+    def writer_textbox_2(self):
+            tm = [0.5, 1,2]
+            count = 0
+            while count < len(self.text_txtbox):
+                if self.letter_textbox_2 != "":
+                    self.sem.acquire()
+                    self.txt_read_2.insert(customtkinter.END, self.letter_textbox_2)
+                    self.letter_textbox_2 = ""
+                    self.sem.release()
+                    time.sleep(tm[random.randint(0, 2)])
+                    count += 1
+            self.txt_read_2.configure(state="disabled")
 
 if __name__ == "__main__":
     app = customtkinter.CTk()
