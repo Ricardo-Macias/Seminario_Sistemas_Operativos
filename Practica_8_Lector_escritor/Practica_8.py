126bdbb9110a1cca608daea20792af878a71b7e5
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-14 21:38:54 -0600

    refactor: quitar la interfaz de la clase

--- Practica_8_Lector_escritor/Practica_8.py
+++ Practica_8_Lector_escritor/Practica_8.py
@@ -9,28 +9,6 @@ class Interfaz:
         self.sem = threading.Semaphore(1)
         self.text_txtbox = ""
         self.letter_textbox = ""
-        self.letter_textbox_2 = ""
-
-        self.txt_writer = customtkinter.CTkTextbox(self.app, width=250, height=400)
-        self.txt_writer.place(x=10, y=50)
-        self.txt_read = customtkinter.CTkTextbox(self.app, width=250, height=400)
-        self.txt_read.place(x=270, y=50)
-        self.txt_read_2 = customtkinter.CTkTextbox(self.app, width=250, height=400)
-        self.txt_read_2.place(x=530, y=50)
-
-        self.btn_read = customtkinter.CTkButton(
-            self.app, text="Leer", width=50, command=self.reader_writer)
-        self.btn_read.place(x=10, y=20)
-        self.btn_save = customtkinter.CTkButton(
-            self.app, text="Guardar", width=50, command=self.save)
-        self.btn_save.place(x=70, y=20)
-        self.btn_edit = customtkinter.CTkButton(
-            self.app, text="Editar", width=50, command=self.edit)
-        self.btn_edit.place(x=140, y=20)
-
-        self.txt_writer.configure(state="disabled")
-        self.txt_read.configure(state="disabled")
-        self.txt_read_2.configure(state="disabled")
 
     def edit(self):
         self.txt_writer.configure(state="normal")
@@ -40,18 +18,11 @@ class Interfaz:
         self.txt_writer.configure(state="disabled")
     
     def reader_writer(self):
-        self.txt_read.configure(state="normal")
-        self.txt_read_2.configure(state="normal")
-
         read = threading.Thread(name="Lector", target=self.reader)
-        read2 = threading.Thread(name="Lector_2", target=self.reader)
         write = threading.Thread(name="Escritor", target=self.writer)
-        write2 = threading.Thread(name="Escritor_2", target=self.writer_textbox_2)
 
         read.start()
-        read2.start()
         write.start()
-        write2.start()
     
     def reader(self):
         tm = [1, 1.5, 2]
@@ -82,19 +53,6 @@ class Interfaz:
                 time.sleep(tm[random.randint(0, 2)])
                 count += 1
         self.txt_read.configure(state="disabled")
-    
-    def writer_textbox_2(self):
-            tm = [0.5, 1,2]
-            count = 0
-            while count < len(self.text_txtbox):
-                if self.letter_textbox_2 != "":
-                    self.sem.acquire()
-                    self.txt_read_2.insert(customtkinter.END, self.letter_textbox_2)
-                    self.letter_textbox_2 = ""
-                    self.sem.release()
-                    time.sleep(tm[random.randint(0, 2)])
-                    count += 1
-            self.txt_read_2.configure(state="disabled")
 
 if __name__ == "__main__":
     app = customtkinter.CTk()
@@ -102,5 +60,28 @@ if __name__ == "__main__":
     app.title("Practica 8: Producto Consumidor")
 
     lector_escritor = Interfaz(app)
+
+    txt_writer = customtkinter.CTkTextbox(app, width=250, height=400)
+    txt_writer.place(x=10, y=50)
+    txt_read = customtkinter.CTkTextbox(
+        app, width=250, height=400)
+    txt_read.place(x=270, y=50)
+    txt_read_2 = customtkinter.CTkTextbox(
+        app, width=250, height=400)
+    txt_read_2.place(x=530, y=50)
+
+    btn_read = customtkinter.CTkButton(
+        app, text="Leer", width=50, command=self.reader_writer)
+    btn_read.place(x=10, y=20)
+    btn_save = customtkinter.CTkButton(
+        app, text="Guardar", width=50, command=self.save)
+    btn_save.place(x=70, y=20)
+    btn_edit = customtkinter.CTkButton(
+        app, text="Editar", width=50, command=self.edit)
+    btn_edit.place(x=140, y=20)
+
+    txt_writer.configure(state="disabled")
+    txt_read.configure(state="disabled")
+    txt_read_2.configure(state="disabled")
 
     app.mainloop()
